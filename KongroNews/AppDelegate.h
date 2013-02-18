996a0a625dff512577b56f21c4a22df9d1074860
Kent Robin <kentrh@gmail.com>
2013-02-18 20:19:32 +0100

    Initial Commit

---
+++ KongroNews/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  KongroNews
+//
+//  Created by Kent Robin Haugen on 18.02.13.
+//  Copyright (c) 2013 Kent Robin Haugen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ KongroNews/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  KongroNews
+//
+//  Created by Kent Robin Haugen on 18.02.13.
+//  Copyright (c) 2013 Kent Robin Haugen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
